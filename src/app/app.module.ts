dd9c827ecf4b59071d09d47ac872a0bb9667a40e
andykeem <andykeem@gmail.com>
2019-12-28 18:41:08 -0500

    Add styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+const appRoutes: Routes = [
+  /*{ path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }*/
+
+  // { path: '', component: HomeComponent },
+  // { path: 'shopping-cart', component: ShoppingCartComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmtFkSvP3oWGx-Vga1FzDZE-r47WUVbSc",
+    authDomain: "oshop-d4378.firebaseapp.com",
+    databaseURL: "https://oshop-d4378.firebaseio.com",
+    projectId: "oshop-d4378",
+    storageBucket: "oshop-d4378.appspot.com",
+    messagingSenderId: "890277153359",
+    appId: "1:890277153359:web:1a9338667afc5446a5b83a",
+    measurementId: "G-Q42E8ETVD1"
+  }
 };
 
 /*
