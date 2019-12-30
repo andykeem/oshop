2428352a807ce2b295c5b05cf650629d92a4033a
andykeem <andykeem@gmail.com>
2019-12-30 17:05:04 -0500

    Hide admin menu to anonymous

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthService } from '../service/auth.service';
-import { Router, NavigationExtras } from '@angular/router';
+import { UserService } from '../service/user.service';
+import { SnapshotAction } from 'angularfire2/database';
+import { AppUser } from '../model/app-user';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent implements OnInit, OnDestroy {
 
   user$: Observable<firebase.User>;
-  admin: boolean;
+  userSubscription: Subscription;
+  isAdmin: boolean;
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private user: UserService) { }
 
+  // Override
   ngOnInit() {
     this.user$ = this.auth.getAuthState();
+    this.userSubscription = this.user$
+      .pipe(
+        switchMap(user => {
+          let snapshot: Observable<SnapshotAction<AppUser>> = this.user.get(user.uid);
+          return snapshot;
+        })
+      ).subscribe(snapshot => {
+        let appUser: AppUser = snapshot.payload.val() as AppUser;
+        this.isAdmin = appUser.isAdmin;
+      });
+  }
+
+  // Override
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 
   logout() {
